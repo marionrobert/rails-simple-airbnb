cc4fcdf188e6ac5f5d769f4b1ca57024b2639636
Marion Robert <mrobert148@gmail.com>
2022-08-17 22:13:30 +0200

    fixing the grid style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating 4 flats..."
+
+4.times do
+  flat = Flat.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(1..10)
+  )
+  flat.save!
+  puts "Create 1 flat"
+end
+puts 'Finished!'
